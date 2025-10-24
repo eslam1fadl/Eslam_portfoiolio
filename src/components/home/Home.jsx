5ef38f749a72ddda7dcef18bd4519a432bd84ec4
eslam1fadl <eslam110@stud.fci-cu.edu.eg>
2025-10-24 07:47:11 +0300

    fix: corrected Skills import case sensitivity

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Hero from './hero'
+import Hero from './Hero'
 import About from './About'
 import Skills from './Skills'
 import FeaturedProjects from './Projects'
 import Contact from './Contact'
+
 
 const Home = () => {
     return (
